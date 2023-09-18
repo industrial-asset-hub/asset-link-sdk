28c94af7be97ab6fb9dd94f10a853b47ec2f2316
Heiko Schabert <heiko.schabert@siemens.com>
2023-09-18 08:33:44 +0200

    feat: provide semantic version of gRPC driver info
    Signed-off-by: Heiko Schabert <heiko.schabert@siemens.com>

--- internal/server/driverinfo/driverinfo.go
+++ internal/server/driverinfo/driverinfo.go
@@ -8,41 +8,50 @@
 package driverinfo
 
 import (
-  generated "code.siemens.com/common-device-management/device-class-drivers/cdm-dcd-sdk/generated/conn_suite_drv_info"
-  "code.siemens.com/common-device-management/device-class-drivers/cdm-dcd-sdk/metadata"
-  "github.com/rs/zerolog/log"
-  "golang.org/x/net/context"
+	generated "code.siemens.com/common-device-management/device-class-drivers/cdm-dcd-sdk/generated/conn_suite_drv_info"
+	"code.siemens.com/common-device-management/device-class-drivers/cdm-dcd-sdk/metadata"
+	semver "github.com/Masterminds/semver/v3"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/net/context"
 )
 
 type DriverInfoServerEntity struct {
-  generated.UnimplementedDriverInfoApiServer
-  Metadata metadata.Metadata
+	generated.UnimplementedDriverInfoApiServer
+	Metadata metadata.Metadata
 }
 
 func (o *DriverInfoServerEntity) GetVersionInfo(c context.Context, request *generated.GetVersionInfoRequest) (*generated.GetVersionInfoResponse, error) {
-  log.Info().Msg("GetVersionInfo called")
-  log.Debug().Interface("Metadata", o.Metadata).Msg("Metadata")
+	log.Info().Msg("GetVersionInfo called")
+	log.Debug().Interface("Metadata", o.Metadata).Msg("Metadata")
 
-  var major uint32 = 1
-  var minor uint32 = 2
-  var patch uint32 = 3
-  var suffix = "unknown"
-  if o.Metadata.Version.Version != "unknown" {
-    // @TODO: Split semver from version
-  }
+	var major uint32 = 0
+	var minor uint32 = 0
+	var patch uint32 = 0
+	var suffix = "unknown"
+	if o.Metadata.Version.Version != "unknown" {
+		parsedVersion, err := semver.NewVersion(o.Metadata.Version.Version)
+		if err != nil {
+			log.Err(err).Str("version string", o.Metadata.Version.Version).Msg("Parsing of Semantic Version")
+		} else {
+			major = uint32(parsedVersion.Major())
+			minor = uint32(parsedVersion.Minor())
+			patch = uint32(parsedVersion.Patch())
+			suffix = parsedVersion.Prerelease()
+		}
+	}
 
-  var product = o.Metadata.DcdName
-  var vendor = o.Metadata.Vendor
-  // Currently not used.
-  var docu = ""
+	var product = o.Metadata.DcdName
+	var vendor = o.Metadata.Vendor
+	// Currently not used.
+	var docu = ""
 
-  return &generated.GetVersionInfoResponse{Version: &generated.VersionInfo{
-    Major:       major,
-    Minor:       minor,
-    Patch:       patch,
-    Suffix:      suffix,
-    VendorName:  vendor,
-    ProductName: product,
-    DocuUrl:     docu,
-  }}, nil
+	return &generated.GetVersionInfoResponse{Version: &generated.VersionInfo{
+		Major:       major,
+		Minor:       minor,
+		Patch:       patch,
+		Suffix:      suffix,
+		VendorName:  vendor,
+		ProductName: product,
+		DocuUrl:     docu,
+	}}, nil
 }
