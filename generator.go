ff099f252f0cac7e27cc938bb0a8d1f8eda9a529
Heiko Schabert <heiko.schabert@siemens.com>
2023-10-18 13:01:10 +0200

    fix: remove generation of firmware update client
    Signed-off-by: Heiko Schabert <heiko.schabert@siemens.com>

--- generator.go
+++ generator.go
@@ -6,9 +6,6 @@ package main
 //go:generate protoc --proto_path=specs --go_out ./generated      --go_opt=Mdevice-discovery.proto=./device_discovery      specs/device-discovery.proto
 //go:generate protoc --proto_path=specs --go-grpc_out ./generated --go-grpc_opt=Mdevice-discovery.proto=./device_discovery specs/device-discovery.proto
 
-//go:generate protoc --proto_path=specs --go_out ./generated      --go_opt=Mfirmware-update.proto=./firmware_update      specs/firmware-update.proto
-//go:generate protoc --proto_path=specs --go-grpc_out ./generated --go-grpc_opt=Mfirmware-update.proto=./firmware_update specs/firmware-update.proto
-
 //go:generate protoc --proto_path=specs --go_out ./generated      --go_opt=Mconn_suite_registry.proto=./conn_suite_registry      specs/conn_suite_registry.proto
 //go:generate protoc --proto_path=specs --go-grpc_out ./generated --go-grpc_opt=Mconn_suite_registry.proto=./conn_suite_registry specs/conn_suite_registry.proto
 
