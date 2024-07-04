ca2d76100ff69ea0cb58d985df01c563e444b13b
rishabh.rana <rana.rishabh@siemens.com>
2024-07-04 15:29:28 +0530

    refactor: remove connection from model
    Signed-off-by: rishabh.rana <rana.rishabh@siemens.com>

--- model/model.go
+++ model/model.go
@@ -24,15 +24,9 @@ func NewDevice(typeOfAsset string) *DeviceInfo {
 type DeviceInfo struct {
 	Type string `json:"@type"`
 	// Override connection point, since generated base schema does not provide derived types
-	ConnectionPoints []Connection `json:"connection_points,omitempty"`
+	ConnectionPoints []any `json:"connection_points,omitempty"`
 	Asset
 	MacIdentifiers []MacIdentifier `json:"mac_identifiers"`
-}
-
-type Connection struct {
-	Ipv4Connectivity Ipv4Connectivity
-	Ipv6Connectivity Ipv6Connectivity
-	EthernetPort     EthernetPort
 }
 
 func CreateTimestamp() string {
