e3a963ef4c9b8094502251146cae20931362f6ab
z004mr8s <arjit.agarwal@siemens.com>
2024-07-12 18:23:03 +0530

    test(helper): add unit test cases

--- model/helper_test.go
+++ model/helper_test.go
@@ -7,8 +7,34 @@
 
 package model
 
-//func TestHelper(t *testing.T) {
-//	t.Run("AddNic", func(t *testing.T) {
-//		t.FailNow()
-//	})
-//}
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestAddIdentifier(t *testing.T) {
+	deviceInfo := NewDevice("testAsset", "test")
+	mac := "test-mac"
+	identifierUncertainty := 1
+	deviceInfo.addIdentifier(mac)
+	assert.Equal(t, len(deviceInfo.MacIdentifiers), 1)
+	assert.Equal(t, deviceInfo.MacIdentifiers[0], MacIdentifier{
+		IdentifierType:        nil,
+		IdentifierUncertainty: &identifierUncertainty,
+		MacAddress:            &mac,
+	})
+}
+
+func TestAddReachabilityState(t *testing.T) {
+	deviceInfo := NewDevice("testAsset", "test")
+	state := ReachabilityStateValuesReached
+	deviceInfo.addReachabilityState()
+	assert.Equal(t, deviceInfo.ReachabilityState.StateValue, &state)
+}
+
+func TestAddManagementState(t *testing.T) {
+	deviceInfo := NewDevice("testAsset", "test")
+	state := ManagementStateValuesUnknown
+	deviceInfo.addReachabilityState()
+	assert.Equal(t, deviceInfo.ManagementState.StateValue, &state)
+}
