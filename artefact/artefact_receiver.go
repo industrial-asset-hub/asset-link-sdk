c501aad99efe00a2fbf088c783dc7d5db5188364
Sebastian Maier <sebastian.maier@siemens.com>
2025-01-08 13:26:47 +0100

    feat: add draft for artefact update API and CLI
    Signed-off-by: Sebastian Maier <sebastian.maier@siemens.com>

---
+++ artefact/artefact_receiver.go
@@ -0,0 +1,97 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Siemens AG
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+package artefact
+
+import (
+	"io"
+	"os"
+	"sync"
+
+	generated "github.com/industrial-asset-hub/asset-link-sdk/v3/generated/artefact-update"
+)
+
+type ArtefactReceiver struct {
+	stream     generated.ArtefactUpdateApi_PushArtefactServer
+	streamLock sync.Mutex
+}
+
+func NewArtefactReceiver(stream generated.ArtefactUpdateApi_PushArtefactServer) *ArtefactReceiver {
+	artefactReceiver := &ArtefactReceiver{stream: stream}
+	return artefactReceiver
+}
+
+func (ar *ArtefactReceiver) ReceiveArtefactChunk() (*generated.ArtefactChunk, error) {
+	ar.streamLock.Lock()
+	defer ar.streamLock.Unlock()
+	return ar.stream.Recv()
+}
+
+func (ar *ArtefactReceiver) ReceiveArtefactToFile(filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return ar.ReceiveArtefactToWriter(file)
+}
+
+func (ar *ArtefactReceiver) ReceiveArtefactToWriter(writer io.Writer) error {
+	for {
+		chunk, err := ar.ReceiveArtefactChunk()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		data := chunk.GetFileContent()
+		lenData := len(data)
+		if lenData > 0 {
+			start := 0
+			for {
+				remainingData := data[start:]
+				n, err := writer.Write(remainingData)
+				if err != nil {
+					return err
+				}
+
+				start += n
+				if start == lenData {
+					break
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func (ar *ArtefactReceiver) ReceiveArtefactToData() (*[]byte, error) {
+	var data []byte
+
+	for {
+		chunk, err := ar.ReceiveArtefactChunk()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+
+		newData := chunk.GetFileContent()
+		data = append(data, newData...)
+	}
+
+	return &data, nil
+}
+
+func (ar *ArtefactReceiver) UpdateStatus(status *generated.ArtefactUpdateStatus) error {
+	ar.streamLock.Lock()
+	defer ar.streamLock.Unlock()
+	return ar.stream.Send(status)
+}
