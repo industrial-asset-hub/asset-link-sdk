9354e9eaf84bdc3758eaf8b8b6980f5eb025cb1b
Sebastian Maier <sebastian.maier@siemens.com>
2025-01-08 13:26:47 +0100

    feat: add draft for artefact update API and CLI
    Signed-off-by: Sebastian Maier <sebastian.maier@siemens.com>

---
+++ artefact/artefact_transmitter.go
@@ -0,0 +1,87 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Siemens AG
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+package artefact
+
+import (
+	"io"
+	"os"
+	"sync"
+
+	generated "github.com/industrial-asset-hub/asset-link-sdk/v3/generated/artefact-update"
+)
+
+type ArtefactTransmitter struct {
+	stream     generated.ArtefactUpdateApi_PullArtefactServer
+	streamLock sync.Mutex
+}
+
+func NewArtefactTransmitter(stream generated.ArtefactUpdateApi_PullArtefactServer) *ArtefactTransmitter {
+	artefactTransmitter := &ArtefactTransmitter{stream: stream}
+	return artefactTransmitter
+}
+
+func (at *ArtefactTransmitter) TransmitArtefactChunk(artefactChunk *generated.ArtefactChunk) error {
+	at.streamLock.Lock()
+	defer at.streamLock.Unlock()
+	return at.stream.Send(artefactChunk)
+}
+
+func (at *ArtefactTransmitter) TransmitArtefactFromFile(filename string, maxChunkSize uint64) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return at.TransmitArtefactFromReader(file, maxChunkSize)
+}
+
+func (at *ArtefactTransmitter) TransmitArtefactFromReader(reader io.Reader, maxChunkSize uint64) error {
+	var data = make([]byte, maxChunkSize)
+	for {
+		n, err := reader.Read(data)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		if n > 0 {
+			actualData := data[:n]
+			chunk := generated.ArtefactChunk{Data: &generated.ArtefactChunk_FileContent{FileContent: actualData}}
+			err = at.TransmitArtefactChunk(&chunk)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (at *ArtefactTransmitter) TransmitArtefactFromData(data *[]byte, maxChunkSize uint64) error {
+	chunkSize := int(maxChunkSize)
+	lenData := len(*data)
+	for start := 0; start < lenData; start += chunkSize {
+		end := start + chunkSize
+
+		if end > lenData {
+			end = lenData
+		}
+
+		chunkData := (*data)[start:end]
+
+		chunk := generated.ArtefactChunk{Data: &generated.ArtefactChunk_FileContent{FileContent: chunkData}}
+		err := at.TransmitArtefactChunk(&chunk)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
