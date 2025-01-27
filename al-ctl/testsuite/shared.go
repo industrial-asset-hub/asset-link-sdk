2f56349dddd6a5543990c5ed28de53ec650bd3dd
z004mr8s <arjit.agarwal@siemens.com>
2025-01-27 16:58:48 +0530

    fix(test-suite): use fmt.sprintf function for strings concatenation

--- al-ctl/testsuite/shared.go
+++ al-ctl/testsuite/shared.go
@@ -66,7 +66,7 @@ func transformSemanticIdentifierToAsset() error {
 		arr := strings.Split(shared.AssetJsonPath, "/")
 		newArr := arr[:len(arr)-1]
 		shared.AssetJsonPath = strings.Join(newArr, "/")
-		shared.AssetJsonPath = shared.AssetJsonPath + fmt.Sprintf("testdevice-%d.json", deviceIndex)
+		shared.AssetJsonPath += fmt.Sprintf("testdevice-%d.json", deviceIndex)
 		// Write the transformed asset to a file
 		jsonWriter := json.NewEncoder(file)
 		if err := jsonWriter.Encode(testDevice); err != nil {
