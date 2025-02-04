b4f6949c254a9d8ce11a207f27ff248a73e9b29d
z004mr8s <arjit.agarwal@siemens.com>
2025-02-04 14:44:27 +0530

    fix(cookiecutter-project-template): remove and colon while assigning value to err at correct places

--- cookiecutter-project-template/{{ cookiecutter.al_project }}/handler/handler.go
+++ cookiecutter-project-template/{{ cookiecutter.al_project }}/handler/handler.go
@@ -74,7 +74,7 @@ func (m *AssetLinkImplementation) Discover(discoveryConfig config.DiscoveryConfi
 	lastSerialNumber.Add(1)
 	serialNumber := fmt.Sprint(lastSerialNumber.Load())
 	productUri := fmt.Sprintf("urn:%s/%s/%s", strings.ReplaceAll(vendorName, " ", "_"), strings.ReplaceAll(product, " ", "_"), serialNumber)
-	err = deviceInfo.AddNameplate(
+	err := deviceInfo.AddNameplate(
 		vendorName,
 		productUri,
 		orderNumber,
@@ -95,7 +95,7 @@ func (m *AssetLinkImplementation) Discover(discoveryConfig config.DiscoveryConfi
 	// Convert and publish device
 	discoveredDevice := deviceInfo.ConvertToDiscoveredDevice()
 
-	err := devicePublisher.PublishDevice(discoveredDevice)
+	err = devicePublisher.PublishDevice(discoveredDevice)
 	if err != nil {
 		// discovery request was likely cancelled -> terminate discovery and return error
 		log.Error().Msgf("Publishing Error: %v", err)
