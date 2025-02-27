c8249e2984c1f696b49a9cf34395f143a461739f
Heiko Schabert <heiko.schabert@siemens.com>
2025-02-27 11:51:36 +0100

    feat: add function add metadata
    Signed-off-by: Heiko Schabert <heiko.schabert@siemens.com>

---
+++ model/metadata.go
@@ -0,0 +1,51 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Siemens AG
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+package model
+
+import (
+	"encoding/base64"
+)
+
+func encodeMetadata(metadata string) string {
+	return base64.StdEncoding.EncodeToString([]byte(metadata))
+}
+
+// TODO: return may an error
+func decodeMetadata(metadata string) string {
+	decoded, err := base64.StdEncoding.DecodeString(metadata)
+	if err != nil {
+		return ""
+	}
+	return string(decoded)
+}
+
+var instanceAnnotationName string = "metadata"
+
+// AddMetadata Add Metadata blob to an asset
+// This acts as persistent storage for certain metadata
+// which are consumed by the Asset Link for e.g. Software Update
+// or other asset management operations
+func (d *DeviceInfo) AddMetadata(metadata string) {
+	metaDataBase64Encoded := encodeMetadata(metadata)
+	// For now stored inside the instance annotations. Should be
+	// moved to a proper field in the future
+	d.InstanceAnnotations = append(d.InstanceAnnotations, InstanceAnnotation{
+		Key:   &instanceAnnotationName,
+		Value: &metaDataBase64Encoded,
+	})
+}
+
+// TODO: return may an error
+func (d *DeviceInfo) getMetadata() string {
+	for _, ia := range d.InstanceAnnotations {
+		if *ia.Key == instanceAnnotationName {
+			return string(*ia.Value)
+		}
+	}
+	return ""
+}
---
+++ model/metadata_test.go
@@ -0,0 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Siemens AG
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+package model
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestEncodeDecode(t *testing.T) {
+	base64Encoded := encodeMetadata("test")
+	assert.Equal(t, "dGVzdA==", base64Encoded)
+	assert.Equal(t, "test", decodeMetadata(base64Encoded))
+}
+
+func TestAddMetaDate(t *testing.T) {
+	d := NewDevice("", "")
+	metadataBlog := "test"
+	d.AddMetadata(metadataBlog)
+	assert.Equal(t, "test", decodeMetadata(d.getMetadata()))
+}
