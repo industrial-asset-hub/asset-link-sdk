d324e61fb39c3d33689979b13e5e80345bea22c1
Heiko Schabert <heiko.schabert@siemens.com>
2025-02-27 14:39:13 +0100

    chore: make method public
    Signed-off-by: Heiko Schabert <heiko.schabert@siemens.com>

--- model/metadata_test.go
+++ model/metadata_test.go
@@ -15,12 +15,12 @@ import (
 func TestEncodeDecode(t *testing.T) {
 	base64Encoded := encodeMetadata("test")
 	assert.Equal(t, "dGVzdA==", base64Encoded)
-	assert.Equal(t, "test", decodeMetadata(base64Encoded))
+	assert.Equal(t, "test", DecodeMetadata(base64Encoded))
 }
 
 func TestAddMetaDate(t *testing.T) {
 	d := NewDevice("", "")
 	metadataBlog := "test"
 	d.AddMetadata(metadataBlog)
-	assert.Equal(t, "test", decodeMetadata(d.getMetadata()))
+	assert.Equal(t, "test", DecodeMetadata(d.getMetadata()))
 }
