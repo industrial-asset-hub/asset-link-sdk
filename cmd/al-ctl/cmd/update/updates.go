2741764cfee5d9cd49cefa3b4f215a81bfd635c0
Sebastian Maier <sebastian.maier@siemens.com>
2025-08-25 12:28:41 +0200

    feat: implement cancel update functionality
    Signed-off-by: Sebastian Maier <sebastian.maier@siemens.com>

--- cmd/al-ctl/cmd/update/updates.go
+++ cmd/al-ctl/cmd/update/updates.go
@@ -14,10 +14,11 @@ import (
 var UpdateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "Update commands",
-	Long:  `Commands related to update management (e.g., firmware updates, software updates). An update consists of two steps: prepare and activate.`,
+	Long:  `Commands related to update management (e.g., firmware updates, software updates). An update consists of two steps: prepare and activate. In addition, a perpared update can be cancelled.`,
 }
 
 func init() {
 	UpdateCmd.AddCommand(UpdatePrepareCommand)
 	UpdateCmd.AddCommand(UpdateActivateCommand)
+	UpdateCmd.AddCommand(UpdateCancelCommand)
 }
