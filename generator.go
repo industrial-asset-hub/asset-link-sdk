893dc0ccd0ebac1aa34b512bc8597f3fe53ac886
Sebastian Maier <sebastian.maier@siemens.com>
2025-10-07 13:32:12 +0200

    feat: add draft for artefact update API and CLI
    Signed-off-by: Sebastian Maier <sebastian.maier@siemens.com>

--- generator.go
+++ generator.go
@@ -19,3 +19,7 @@ package main
 // Connectivity Suite Discover Interface
 //go:generate protoc --proto_path=specs --go_out      ./generated --go_opt=Miah_discover.proto=./iah-discovery --go_opt=Mcommon_address.proto=./iah-discovery --go_opt=Mcommon_variant.proto=./iah-discovery --go_opt=Mcommon_operators.proto=./iah-discovery --go_opt=Mcommon_filters.proto=./iah-discovery --go_opt=Mcommon_identifiers.proto=./iah-discovery --go_opt=Mcommon_code.proto=./iah-discovery iah_discover.proto common_address.proto common_variant.proto common_operators.proto common_filters.proto common_identifiers.proto common_code.proto
 //go:generate protoc --proto_path=specs --go-grpc_out ./generated --go-grpc_opt=Miah_discover.proto=./iah-discovery --go-grpc_opt=Mcommon_address.proto=./iah-discovery --go-grpc_opt=Mcommon_variant.proto=./iah-discovery --go-grpc_opt=Mcommon_operators.proto=./iah-discovery --go-grpc_opt=Mcommon_filters.proto=./iah-discovery --go-grpc_opt=Mcommon_identifiers.proto=./iah-discovery --go-grpc_opt=Mcommon_code.proto=./iah-discovery iah_discover.proto
+
+// Artefact Update Interface
+//go:generate protoc --proto_path=specs --go_out      ./generated  --go_opt=Martefact_update.proto=./artefact-update artefact_update.proto
+//go:generate protoc --proto_path=specs --go-grpc_out ./generated  --go-grpc_opt=Martefact_update.proto=./artefact-update artefact_update.proto
