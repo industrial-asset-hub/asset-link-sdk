e7a9b6e3068dd0a033a744d3b43431dfc3c0030b
swati.shinghal@siemens.com <swati.shinghal@siemens.com>
2025-12-15 16:39:54 +0530

    fix: fetching the latest version for command line tool
    Signed-off-by: swati.shinghal@siemens.com <swati.shinghal@siemens.com>

--- cookiecutter-project-template/{{ cookiecutter.al_project }}/testscripts/get_command_line_tool.sh
+++ cookiecutter-project-template/{{ cookiecutter.al_project }}/testscripts/get_command_line_tool.sh
@@ -13,15 +13,8 @@ if [[ -f al-ctl ]]; then
 
 else
     echo "al-ctl not found. Downloading..."
-    # Check asset-link-sdk current version present in asset link using go list
-    get_version=$(go list -m -f '{{.Version}}' github.com/industrial-asset-hub/asset-link-sdk/v3)
-    echo "Asset Link SDK Version: ${get_version}"
-    # If go list fails, fallback to hardcoded version
-    if [[ -z "${get_version}" ]]; then
-        get_version="v3.6.2"
-    fi
-
-    curl -L -o al-ctl_${OS_NAME}_${ARCH_NAME}.tar.gz https://github.com/industrial-asset-hub/asset-link-sdk/releases/download/${get_version}/al-ctl_${OS_NAME}_${ARCH_NAME}.tar.gz
+    
+    curl -L -o al-ctl_${OS_NAME}_${ARCH_NAME}.tar.gz https://github.com/industrial-asset-hub/asset-link-sdk/releases/latest/download/al-ctl_${OS_NAME}_${ARCH_NAME}.tar.gz
     tar -xf al-ctl_${OS_NAME}_${ARCH_NAME}.tar.gz
 
 fi
